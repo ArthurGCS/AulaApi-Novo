d25a7d7af179fef3d40336b5c2db80f88c3b9357
ArthurGCS <art.guilher@gmail.com>
2025-06-16 20:35:23 -0300

    Adicionar arquivos de projeto.

---
+++ AulaApiDevOpsManhaPiaget/Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace AulaApiDevOpsManhaPiaget.Models
+{
+    public class Fornecedor : Base
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+        public int ProdutosId { get; set; }
+    }
+}
